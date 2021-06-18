9a1a00062f4b7081f1413302507c6f7ed2969bfb
samuelwondimu <wsamuelwondimu@gmail.com>
2021-06-18 11:20:27 +0300

    fix folder structre

--- modules/feeds/feed.model.ts
+++
@@ -1,8 +0,0 @@
-import { ModificationNote } from "../common/model";
-
-export interface IPost {
-  _id?: String;
-  caption: String;
-  image: String;
-  modification_notes: [ModificationNote];
-}
--- server.ts
+++
@@ -1,8 +0,0 @@
-import app from "./config/app";
-import env from "./environment";
-
-const PORT = 3000;
-
-app.listen(PORT, () => {
-  console.log("Express server listening on port " + PORT);
-});
