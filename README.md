39e65e0eeb70d62170c791528774d5b02d1d5971
samuelwondimu <wsamuelwondimu@gmail.com>
2021-06-19 22:57:05 +0300

    add token reuire to user route

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # project_typescript
+
 REST_API with typescript/express - MongoDB - Node.Js
+
+# Auth-Routes
+
+POST - /v1/auth - regiester account
+POST - /v1/auth/sigin - login account
+GET - /v1/auth/validate - validate account - authorization token required
+GET - /v1/auth/all - get all account
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,22 +1,23 @@
 import { Application, Request, Response } from "express";
 import { UserController } from "../modules/users/user.controller";
+import extractJWT from "../middleware/extractJWT";
 
 export class userRoutes {
   private user_controller: UserController = new UserController();
 
   public route(app: Application) {
     // Create a user
-    app.post("/v1/users", (req: Request, res: Response) => {
+    app.post("/v1/users", extractJWT, (req: Request, res: Response) => {
       this.user_controller.create_user(req, res);
     });
 
     // Get users
-    app.get("/v1/users", (req: Request, res: Response) => {
+    app.get("/v1/users", extractJWT, (req: Request, res: Response) => {
       res.status(200).json({ message: "Get users" });
     });
 
     // Get user by :_id
-    app.get("/v1/users/:id", (req: Request, res: Response) => {
+    app.get("/v1/users/:id", extractJWT, (req: Request, res: Response) => {
       res.status(200).json({ message: "Get users" });
     });
   }
