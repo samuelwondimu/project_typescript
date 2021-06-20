5bd40ffff485b4612165b06d9b8621413421462f
samuelwondimu <wsamuelwondimu@gmail.com>
2021-06-20 13:21:34 +0300

    port

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import app from "./config/app";
 import config from "./config/config";
 
 const NAMESPACE = "Server";
-const PORT = config.server.port || 5000;
+const PORT = config.server.port;
 
 app.listen(PORT, () => {
   console.log("Express server listening on port " + PORT);
