a289b4643aca8ecf4c2562c1a2b0db843f58f19a
samuelwondimu <wsamuelwondimu@gmail.com>
2021-06-20 13:33:08 +0300

    port error

--- src/config/config.ts
+++ src/config/config.ts
@@ -20,7 +20,7 @@ const MONGO = {
 };
 
 const SERVER_HOSTNAME = "localhost";
-const SERVER_PORT = process.env.SERVER_PORT;
+const SERVER_PORT = process.env.SERVER_PORT || 5000;
 const SERVER_TOKEN_EXPIRETIME = process.env.SERVER_TOKEN_EXPIRETIME || 3600;
 const SERVER_TOKEN_ISSUER = process.env.SERVER_TOKEN_ISSUER || "coolIssuer";
 const SERVER_TOKEN_SECRET =
